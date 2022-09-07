75fc89e2318badc61a4913e46a556602c75b2745
Salim Afiune <afiune@lacework.net>
2022-09-07 12:52:10 -0700

    fix: examples/ google constraint (#35)
    Error: Failed to query available provider packages
    
    Could not retrieve the list of available versions for provider
    hashicorp/google: no available releases match the given constraints >= 3.0.0,
    ~> 3.0, >= 4.4.0, < 5.0.0
    
    Error: Could not load plugin
    Signed-off-by: Salim Afiune Maya <afiune@lacework.net>
    
    Signed-off-by: Salim Afiune Maya <afiune@lacework.net>

--- examples/default/versions.tf
+++ examples/default/versions.tf
@@ -1,8 +1,7 @@
 terraform {
-  required_version = ">= 0.12.26"
+  required_version = ">= 0.14"
 
   required_providers {
-    google = "~> 3.0"
     lacework = {
       source = "lacework/lacework"
     }
