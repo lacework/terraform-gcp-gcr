75d4c3b398ab41691cdae57af3a1ce1a1f05d4e8
Darren Murray <darren.murray@lacework.net>
2022-11-09 09:57:53 +0000

    fix: pin max google provider version to 4.41.0
    Signed-off-by: Darren Murray <darren.murray@lacework.net>

--- versions.tf
+++ versions.tf
@@ -2,11 +2,11 @@ terraform {
   required_version = ">= 0.14"
 
   required_providers {
-    google = ">= 3.0.0, < 5.0.0"
+    google = ">= 3.0.0, < 4.41.0"
     time   = "~> 0.6"
     lacework = {
       source  = "lacework/lacework"
-      version = "~> 1.0"
+      version = "~> 0.3"
     }
   }
 }
